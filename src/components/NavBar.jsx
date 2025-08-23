fad0f69a158e4f0e28014f3e07796c69f614bb14
Manuel Isaias Navarro Montealegre <manuelinm2015@gmail.com>
2025-08-23 01:06:49 -0500

    Fx: Navbar fixed to top

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const NavBar = () => {
     return (
-        <header className="bg-white shadow-sm border-b border-gray-200 ">
+        <header className="bg-white fixed top-0 w-full z-50 shadow-sm border-b border-gray-200 ">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between items-center h-16">
                     <div className="flex items-center space-x-3">
