ac656ccdacee0af9dc25f1f2840726fb432dbaf2
Manuel Isaias Navarro Montealegre <manuelinm2015@gmail.com>
2025-08-23 13:18:27 -0500

    prueba 3

--- src/libs/zod.ts
+++ src/libs/zod.ts
@@ -2,10 +2,10 @@
 import { object, string } from "zod";
 
 export const signInSchema = object({
-  email: string({ required_error: "El correo es obligatorio" })
+  email: string({ message: "El correo es obligatorio" })
     .min(1, "El correo es obligatorio")
     .email("Correo inválido"),
-  password: string({ required_error: "La contraseña es obligatoria" })
+  password: string({ message: "La contraseña es obligatoria" })
     .min(8, "La contraseña debe tener al menos 8 caracteres")
     .max(32, "La contraseña debe tener menos de 32 caracteres"),
 });
