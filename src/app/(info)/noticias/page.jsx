e7593370badb93d81e7e9b6a9cb7ec9e4b4c7790
Jhon Pulido <jhonpulido762@gmail.com>
2025-08-27 10:07:39 -0500

    FIX: Ya las noticias son dinámicas y hay paginación incluída

--- src/app/(info)/noticias/page.jsx
+++ src/app/(info)/noticias/page.jsx
@@ -2,77 +2,59 @@
 import NewsCard from '@/components/NewsCard'
 import React, { useEffect, useState } from 'react'
 import Loading from './Loading'
+import Paginator from '@/components/Paginator'
 
 const Page = () => {
     const [loading, setLoading] = useState(true);
     const [card, setCard] = useState([]);
     const [error, setError] = useState(false);
+    const [pagination, setPagination] = useState({
+        page: 1,
+        limit: 6,
+        totalCount: 0,
+        totalPages: 1,
+    });
+
+    const fetchLatestNews = async () => {
+        setLoading(true)
+        try {
+            const response = await fetch(`${process.env.NEXT_PUBLIC_URL_BACKEND}/news`);
+            const result = await response.json();
+
+            const { data } = result;
+            const formattedNews = data.news.map((news) => ({
+                id: news.id,
+                title: news.title,
+                content: news.content,
+                image: news.images?.[0] || null,
+                link: `/noticias/detalles/${news.id}`,
+            }));
+
+            setCard(formattedNews);
+            setPagination(data.pagination);
+        } catch (error) {
+            console.error('Error al obtener las noticias:', error.message);
+            setError(true);
+        } finally {
+            setLoading(false);
+        }
+    };
+
 
     useEffect(() => {
-        const fetchLatestNews = async () => {
-            try {
-                const data = {
-                    data: {
-                        latestNews: [
-                            {
-                                id: 1,
-                                title: "Lanzamiento del nuevo programa social",
-                                content: "Se ha lanzado un nuevo programa para apoyar a jóvenes emprendedores.",
-                            },
-                            {
-                                id: 2,
-                                title: "Conferencia internacional en la ciudad",
-                                content: "Expertos de todo el mundo se reúnen para debatir sobre innovación.",
-                            },
-                            {
-                                id: 3,
-                                title: "Iniciativa verde para la comunidad",
-                                content: "Se inicia un plan de reforestación en parques y zonas verdes.",
-                            },
-                            {
-                                id: 4,
-                                title: "Nueva plataforma educativa",
-                                content: "Se lanza una plataforma para el aprendizaje en línea.",
-                            },
-                            {
-                                id: 5,
-                                title: "Mejoras en el transporte público",
-                                content: "El sistema de transporte implementa nuevas rutas sostenibles.",
-                            },
-                            {
-                                id: 6,
-                                title: "Festival cultural en el centro",
-                                content: "Se celebrará un festival con gastronomía, música y arte local.",
-                            }
-                        ]
-                    }
-                };
+        fetchLatestNews(pagination.page);
+    }, [pagination.page]);
 
-                const formattedNews = data.data.latestNews.map((news) => ({
-                    id: news.id,
-                    title: news.title,
-                    content: news.content,
-                    image: news.image_urls?.[0] || null,
-                    link: `/detailedNotice/${news.id}`,
-                }));
 
-                setCard(formattedNews);
-            } catch (error) {
-                console.error('Error al obtener las noticias:', error.message);
-                setError(true);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchLatestNews();
-    }, []);
+    const handlePageChange = (page) => {
+        setPagination((prev) => ({ ...prev, page }));
+    };
 
     return (
         <div className=' bg-gray-50 py-12 px-6 pt-24'>
             <div className="flex flex-col text-center">
-                <h1 className="text-lg md:text-5xl font-bold text-blue-800 mb-4 text-center">Noticias de ultimo momento</h1>
-                <p className='text-md text-gray-600 max-w-3xl mx-auto mb-5'>Enterate de las ultimas noticias publicadas en nuestro sitio web</p>
+                <h1 className="text-lg md:text-5xl font-bold text-blue-800 mb-4 text-center">Noticias de último momento</h1>
+                <p className='text-md text-gray-600 max-w-3xl mx-auto mb-5'>Entérate de las últimas noticias publicadas en nuestro sitio web</p>
             </div>
             <div className="w-full my-5">
                 {loading ? (
@@ -83,7 +65,7 @@ const Page = () => {
                     </div>
                 ) : (
                     <div className="flex flex-col mx-auto p-4 w-full max-w-5xl">
-                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mb-8">
                             {card.map((newsItem) => (
                                 <NewsCard
                                     key={newsItem.id}
@@ -95,6 +77,12 @@ const Page = () => {
                                 />
                             ))}
                         </div>
+                        <Paginator
+                            totalItems={pagination.totalCount}
+                            itemsPerPage={pagination.limit}
+                            currentPage={pagination.page}
+                            onPageChange={handlePageChange}
+                        />
                     </div>
                 )}
 
